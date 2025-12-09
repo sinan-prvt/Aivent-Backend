630bccc234e7a77c1d3d18f8c4e9abc56f562d2d
sinan-prvt <mohamedsinan9400@gmail.com>
2025-12-09 10:25:17 +0530

    auth_app.tasks: remove sync_user_to_user_service_task

--- auth-service/auth_app/tasks.py
+++ auth-service/auth_app/tasks.py
@@ -3,27 +3,8 @@ from django.core.mail import send_mail
 from django.conf import settings
 import logging
 from .rabbit import publish_event
-import requests
 
 logger = logging.getLogger(__name__)
-
-
-@shared_task 
-def sync_user_to_user_service_task(user_data): 
-    url = settings.USER_SERVICE_URL.rstrip("/") + "/api/internal/users/" 
-    token = settings.USER_SERVICE_INTERNAL_TOKEN 
-    
-    try: 
-        resp = requests.post( 
-            url, 
-            json=user_data, 
-            headers={"Authorization": f"Bearer {token}"}, 
-            timeout=5 
-        ) 
-        return {"status": resp.status_code, "response": resp.text} 
-    except Exception as e: 
-        return {"status": "error", "message": str(e)}
-
 
 
 @shared_task(bind=True, max_retries=3, default_retry_delay=60)
